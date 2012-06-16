601c957ffdeb72d48ebf92d4aee494bfc9c6eb7e
lezcano.da@gmail.com <lezcano.da@gmail.com@c3038d21-ecf1-1489-44a9-52e1197a981f>
2012-06-16 23:17:29 +0000

    CU24-28 finalizado (secuencia, pantalla y modelo) faltan cu prueba. Tambien modifique diag de dominio y doc de arquitectura con las correcciones planteadas por el prosor!..

--- se/Trabajo Profesional/Disciplinas/4.Diseño/diagramas/budytraining/web/GroupController.java
+++ se/Trabajo Profesional/Disciplinas/4.Diseño/diagramas/budytraining/web/GroupController.java
@@ -7,15 +7,44 @@ import model.TrainingLog;
 
 public class GroupController {
 	
+	private List<TrainingLog> trainingLogs;
+	
 	public void selectSportsman(Sportsman sportsman){
 		
 	}
 	
-	public void createSpeedRegisters(List<TrainingLog> trainingLogs){
-		
-	}
-	
-	public List<GraphItemView> getSpeedRegisters(){
+	public List<GraphItemView> generateSpeedRegisters(){
 		return null;
 	}
+	
+	public List<GraphItemView> generateResistenceRegisters(){
+		return null;
+	}
+
+	public List<GraphItemView> generateWeightRegisters(){
+		return null;
+	}
+
+	public List<GraphItemView> generateForceRegisters(){
+		return null;
+	}
+
+	//Biceps
+	public List<GraphItemView> generateMusclesBicepsRegisters(){
+		return null;
+	}
+	
+	//Triceps
+	public List<GraphItemView> generateTricepsBicepsRegisters(){
+		return null;
+	}
+
+	public List<TrainingLog> getTrainingLogs() {
+		return trainingLogs;
+	}
+
+	public void setTrainingLogs(List<TrainingLog> trainingLogs) {
+		this.trainingLogs = trainingLogs;
+	}
+	
 }
