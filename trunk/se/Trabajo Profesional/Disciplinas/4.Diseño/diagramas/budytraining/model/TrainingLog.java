8ea8295bc304b94b9bfc73b87eae6d5da4badda7
lezcano.da@gmail.com <lezcano.da@gmail.com@c3038d21-ecf1-1489-44a9-52e1197a981f>
2012-06-16 20:20:16 +0000

    CU24 finalizado (secuencia, pantalla y modelo) falta cu prueba.

--- trunk/se/Trabajo Profesional/Disciplinas/4.Diseño/diagramas/budytraining/model/TrainingLog.java
+++ trunk/se/Trabajo Profesional/Disciplinas/4.Diseño/diagramas/budytraining/model/TrainingLog.java
@@ -9,5 +9,30 @@ public class TrainingLog {
 	private List<IActivity> activities;
 	
 	private SportsmanAttribute sportsmanAttribute;
+
+	//---
+	public Date getLogDate() {
+		return logDate;
+	}
+
+	public void setLogDate(Date logDate) {
+		this.logDate = logDate;
+	}
+
+	public List<IActivity> getActivities() {
+		return activities;
+	}
+
+	public void setActivities(List<IActivity> activities) {
+		this.activities = activities;
+	}
+
+	public SportsmanAttribute getSportsmanAttribute() {
+		return sportsmanAttribute;
+	}
+
+	public void setSportsmanAttribute(SportsmanAttribute sportsmanAttribute) {
+		this.sportsmanAttribute = sportsmanAttribute;
+	}
 	
 }
---
+++ trunk/se/Trabajo Profesional/Disciplinas/4.Diseño/diagramas/budytraining/web/GroupController.java
@@ -0,0 +1,21 @@
+package web;
+
+import java.util.List;
+
+import model.Sportsman;
+import model.TrainingLog;
+
+public class GroupController {
+	
+	public void selectSportsman(Sportsman sportsman){
+		
+	}
+	
+	public void createSpeedRegisters(List<TrainingLog> trainingLogs){
+		
+	}
+	
+	public List<GraphItemView> getSpeedRegisters(){
+		return null;
+	}
+}
