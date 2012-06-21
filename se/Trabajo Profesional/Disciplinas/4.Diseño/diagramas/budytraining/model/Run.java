894eedcfab064b00cd5f06fdfe6d3df533cb2b4c
ezequiel.fernandez@gmail.com <ezequiel.fernandez@gmail.com@c3038d21-ecf1-1489-44a9-52e1197a981f>
2012-06-21 03:17:15 +0000

    Se crea secuencia del CU15

--- se/Trabajo Profesional/Disciplinas/4.Diseño/diagramas/budytraining/model/Run.java
+++ se/Trabajo Profesional/Disciplinas/4.Diseño/diagramas/budytraining/model/Run.java
@@ -1,5 +1,13 @@
 package model;
 
+import java.util.List;
+
 public class Run implements IActivity {
+
+	@Override
+	public List<IActivity> getActivity() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
