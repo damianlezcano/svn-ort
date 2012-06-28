feac3dc02799afc02ebaeffeef373141e70441b5
arratta.s@gmail.com <arratta.s@gmail.com@c3038d21-ecf1-1489-44a9-52e1197a981f>
2012-06-28 21:28:23 +0000

    CU30 - Registrando datos de actividad individual COMPLETO

---
+++ se/Trabajo Profesional/Disciplinas/4.Diseño/diagramas/budytraining/web/RegisteringIndividualActivityController.java
@@ -0,0 +1,15 @@
+package web;
+
+import service.SportsmanServiceBean;
+import model.User;
+
+
+public class RegisteringIndividualActivityController {
+	
+	private User user;
+	private SportsmanServiceBean sportsmanServiceBean;
+	
+	
+
+}
+
