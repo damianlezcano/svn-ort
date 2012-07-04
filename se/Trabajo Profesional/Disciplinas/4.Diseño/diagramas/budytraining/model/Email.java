4e98b2931e763da874dfcb8395f1f974c0e8c519
estebanarlando@gmail.com <estebanarlando@gmail.com@c3038d21-ecf1-1489-44a9-52e1197a981f>
2012-07-04 03:02:53 +0000

    Se crea la clase Email se agrega método sobrecargado en EmailServiceBean se agrega método en GraphItemView para tomar el archivo Se crea diagrama de secuencia

---
+++ se/Trabajo Profesional/Disciplinas/4.Diseño/diagramas/budytraining/model/Email.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.io.File;
+
+public class Email {
+	private String from;
+	private String to;
+	private String subject;
+	private String text;
+	private File attach;
+	
+	public Email (String to, String Subject) {
+		
+	}
+
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public File getAttach() {
+		return attach;
+	}
+	public void setAttach(File attach) {
+		this.attach = attach;
+	}
+
+
+}
--- se/Trabajo Profesional/Disciplinas/4.Diseño/diagramas/budytraining/service/EmailServiceBean.java
+++ se/Trabajo Profesional/Disciplinas/4.Diseño/diagramas/budytraining/service/EmailServiceBean.java
@@ -1,10 +1,14 @@
 package service;
 
+import model.Email;
 import model.User;
 
 public class EmailServiceBean {
 
 	public void send(User user, String body) {
+	}
+	public void send(Email email) {
+		
 	}
 	
 }
